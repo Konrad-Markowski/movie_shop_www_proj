a06291a399430d457ee968b44396774207cc51c4
Konrad Markowski <k.markowski2002@gmail.com>
2025-01-15 14:04:30 +0100

    updated cfg.php path

--- php/product.php
+++ php/product.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shop";
-
-// Połączenie z bazą danych
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once('../cfg.php');
 
 // Pobranie danych produktu
 $product_id = $_GET['id'] ?? 0;
--- php/register.php
+++ php/register.php
@@ -1,15 +1,6 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shop";
-
-// Połączenie z bazą danych
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once('../cfg.php');
 
 // Obsługa formularza rejestracji
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
